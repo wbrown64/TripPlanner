2abd258a9b9321f8bd4105bb1351a4f2fac97b63
athai <athai@madrid.cs.colostate.edu>
2017-02-16 17:49:24 -0700

    Uploading basic methods

--- src/View/View.java
+++ src/View/View.java
@@ -6,9 +6,34 @@ public class View {
 		
 	}
 	
+	/*public void initializeTrip(stream,stream){
+		//implement
+	}*/
+	
 	public static String display(String text){
 		//System.out.println(text);
 		return text;
+	}
+	public void addLeg(int seq,String start,String end, String mileage){
+	//implement	
+	}
+	
+	public void addLine(double x1, double y1, double x2, double y2){
+		//implement
+	}
+	
+	//public void addLabel(double)
+	
+	public void addHeader(String title){
+		//implement
+	}
+	
+	public void addFooter(String foot){
+		//implement
+	}
+	
+	public void finalizeTrip(){
+		//implement
 	}
 	
 	public static void main(String args[]){
